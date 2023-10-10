cea2a117962691f0a9ed8205b9828f6a568c3c6f
Eduwill <eduwill100@gmail.com>
2023-10-10 01:49:30 -0300

    Refactor Main class for readability and modularity

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -12,12 +12,24 @@ class Main {
    */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    $htmlFilePath = '/../../assets/origin.html';
 
-    $data = (new Scrapper())->scrap($dom);
+    $dom->loadHTMLFile(__DIR__ . $htmlFilePath);
 
-    // Write your logic to save the output file bellow.
+    $scrapper = new Scrapper();
+    $data = $scrapper->scrap($dom);
+    // Print scraped data
     print_r($data);
+
+    // Save scraped data as JSON, formatting for readability
+    $jsonFilePath = __DIR__ . './scraped_data.json';
+    file_put_contents($jsonFilePath, json_encode($data, JSON_PRETTY_PRINT));
+    
+    // Set XLSX file path and export data to XLSX.
+    $outputFilePath = './assets/output.xlsx';
+    $scrapper->exportToXlsx($data, $outputFilePath);
+    echo 'XLSX file created successfully at ' . $outputFilePath . PHP_EOL;
+
   }
 
 }
