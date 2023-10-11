79eac4123bfa6d8a6876923c6d17e40c23c056ba
Eduwill <eduwill100@gmail.com>
2023-10-11 15:42:43 -0300

    update

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/side-bar/side-bar.component';  // Corrected import path
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
